944bd3ec469148180f368848b449f5641940c34c
ori0o0p <openwund8517@gmail.com>
2023-07-10 11:13:25 +0900

    add

---
+++ src/main/java/com/example/icetabetaiBackend/icetabetaiProjectApplication.java
@@ -0,0 +1,12 @@
+package com.example.icetabetaiBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class icetabetaiProjectApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(icetabetaiProjectApplication.class, args);
+    }
+
+}
